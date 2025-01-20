d1a9e586a044419bed6dbc51d9aa9a2ca480f9b9
Nitish sahni <nitesh701saini@gmail.com>
2025-01-20 09:48:08 -0400

    server.js

--- server.js
+++ server.js
@@ -9,4 +9,19 @@ app.get("/sum" , function(res ,req){
     res.setEncoding(sum.toString());
 });
 
-app.listen(4000)
+
+app.get("/interest" , (res , req)=>{
+    const principal = parseInt(req.query.principal);
+    const rate      = parseInt(req.query.rate);
+    const time      = parseInt(req.query.time);
+    const interest  = parseInt(req.query.interest);
+    const total     = principal + interest;
+
+    res.setEncoding({
+        total : total,
+        interest: interest,
+    })
+});
+
+
+app.listen(8080)
